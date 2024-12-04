518530bd84d7cdd0d0fe4193e96e510a12e10dda
DanielSchn <d.schneider@dsc-mail.de>
2024-12-04 13:56:39 +0100

    add registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { EmailVerificationComponent } from './email-verification/email-verificat
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     { path: 'verify-email', component: EmailVerificationComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'login', component: LoginComponent },
     { path: 'password-reset', component: PasswordResetComponent },
+    { path: 'sign-up', component: SignupComponent },
 ];
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ConstantsService } from '../constants.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterModule,
+    CommonModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+  isPasswordMismatch: boolean = false;
+  isButtonDisabled: boolean = true;
+  passwordTooShort: boolean = false;
+  password: string = '';
+  confirmPassword: string = '';
+  email: string = '';
+  message: string = '';
+  error: string = '';
+  
+  constructor(
+    private http: HttpClient,
+    private constants: ConstantsService,
+    private toastr: ToastrService,
+    private router: Router
+  ){}
+
+
+
+
+  checkPasswordMatch(): void {
+    this.isPasswordMismatch =
+      !!this.password.trim() &&
+      !!this.confirmPassword.trim() &&
+      this.password !== this.confirmPassword;
+  
+    this.updateButtonStatus();
+  }
+
+  updateButtonStatus(): void {
+    this.isButtonDisabled =
+      !this.password.trim() || 
+      !this.confirmPassword.trim() || 
+      this.isPasswordMismatch;
+  }
+
+  checkPasswordLength() {
+     return this.password.length <= 7;
+  }
+
+  signUp() {
+    if (this.checkPasswordLength()) {
+      this.passwordTooShort = true;
+      return;
+    } else {
+      if (this.email && this.password) {
+        this.http.post(this.constants.API_BASE_URL + 'api/registration/', {
+          email: this.email,
+          password: this.password,
+          confirm_password: this.confirmPassword,
+        }).subscribe({
+          next: (response: any) => {
+            this.message = response.message;
+            this.error = '';
+            this.toastr.success(this.message, 'Success', {
+              positionClass: this.constants.TOASTR_POSITION,
+              timeOut: this.constants.TOASTR_TIMEOUT
+            });
+          },
+          error: (error) => {
+            this.error = error.error?.error || 'Error creating account.';
+            this.message = '';
+            this.toastr.error(this.error, 'Error', {
+              positionClass: this.constants.TOASTR_POSITION,
+              timeOut: this.constants.TOASTR_TIMEOUT
+            });
+          },
+        });
+      } else {
+        this.error = 'Invalid link.';
+        this.toastr.error(this.error, 'Error', {
+          positionClass: this.constants.TOASTR_POSITION,
+          timeOut: this.constants.TOASTR_TIMEOUT
+        });
+      }
+      setTimeout(() => {
+        this.router.navigate(['/']);
+      }, 3500);
+    }
+  }
+  
+}
