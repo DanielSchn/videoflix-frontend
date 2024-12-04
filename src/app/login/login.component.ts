3f0d16652840691c1b1f0229041aa72639e5f5fb
DanielSchn <d.schneider@dsc-mail.de>
2024-12-04 12:52:32 +0100

    login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    FormsModule,
+    CommonModule,
+    RouterModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  passwordVisible: boolean = false;
 
+  login() {
+
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
 }
