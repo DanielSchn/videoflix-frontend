77a789f109d611b529dc1dc38501f73fec329531
DanielSchn <d.schneider@dsc-mail.de>
2024-12-13 10:34:54 +0100

    videogular at preview banner

--- src/app/video-list/preview-banner/preview-banner.component.ts
+++ src/app/video-list/preview-banner/preview-banner.component.ts
@@ -2,12 +2,20 @@ import { Component, Input } from '@angular/core';
 import { VideoService } from '../../video.service';
 import { ConstantsService } from '../../constants.service';
 import { CommonModule } from '@angular/common';
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
 
 @Component({
   selector: 'app-preview-banner',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   templateUrl: './preview-banner.component.html',
   styleUrl: './preview-banner.component.scss'
