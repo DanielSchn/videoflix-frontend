f14a89fba4c399c64a3a00c4b7cff429c1fe314a
DanielSchn <d.schneider@dsc-mail.de>
2024-12-19 12:10:31 +0100

    create privacy and legal

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-datenschutz',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    FooterComponent
+  ],
   templateUrl: './datenschutz.component.html',
   styleUrl: './datenschutz.component.scss'
 })
--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    FooterComponent
+  ],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss'
 })
