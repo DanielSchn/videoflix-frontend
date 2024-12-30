1166373039330c7b344a6d03cd829c6ca6dcfc51
DanielSchn <d.schneider@dsc-mail.de>
2024-12-30 18:23:42 +0100

    remove environments

--- src/environments/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    API_BASE_URL: 'http://127.0.0.1:8000/',
-    API_MEDIA_URL: 'http://127.0.0.1:8000',
-    TOASTR_TIMEOUT: 3000,
-    TOASTR_POSITION: 'toast-bottom-left',
-    CATEGORIES: ['documentary', 'crime', 'sports', 'romance']
-  };
